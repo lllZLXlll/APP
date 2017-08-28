cfd99aa69a3f54a68e58da36d254a56adf30a809
IIIZLXIII <zlx_ooo@126.com>
2017-08-28 15:32:08 +0800

    我的-发帖模块
    我的-发帖模块，昨晚一部分页面

--- App/app/resources/user/article.js
+++ App/app/resources/user/article.js
@@ -6,15 +6,61 @@ import {
 	View,
 	Text,
 	Image,
+	ScrollView,
+	TouchableOpacity,
 } from 'react-native';
+
+import Styles from '../../style/user/userStyle';
+
+const itemSelectIcon = require('../../images/icon/icon_1.png');
 
 export default class Article extends Component {
 
+	_getArticleView() {
+		return 	<View style={Styles.articleView}>
+					<View style={Styles.articleTopView}>
+						<View style={Styles.textLeftView}>
+							<Text style={Styles.textLeft}>发帖：2</Text>
+						</View>
+						<View style={Styles.textRightView}>
+							<Text style={Styles.textRight}>编辑</Text>
+						</View>
+					</View>
+				</View>;
+	}
+
+	_getActicleItemView() {
+		return 	<View style={Styles.articleItemView}>
+					<View style={Styles.itemTopView}>
+						<View style={Styles.itemTopLeftView}>
+							<Text style={Styles.itemTopLeftDate}>2017-08-18 13:53
+								<Text style={[Styles.itemTopLeftDate, {color: '#ff8200'}]}> 发布成功，粉丝将收到您的发帖通知！</Text>
+							</Text>
+						</View>
+						<View style={Styles.itemTopRightView}>
+							<TouchableOpacity activeOpacity={1} onPress={() => {alert('选中')}}>
+								<Image style={Styles.itemSelectIcon} source={itemSelectIcon} />
+							</TouchableOpacity>
+						</View>
+					</View>
+					<View style={Styles.itemConentView}>
+						<Text style={Styles.itemConentText}>
+							舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担
+							舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担舒服的沙发上的史蒂夫史蒂夫史蒂夫都是负担
+						</Text>
+					</View>
+					<View style={Styles.itemPraiseView}>
+						
+					</View>
+				</View>;
+	}
+
 	render() {
 		return (
-			<View style={{width: 200, height: 200, backgroundColor: '#fff', alignItems: 'center', marginTop: 40}}>
-				<Text>发帖</Text>
-			</View>
+			<ScrollView>
+				{ this._getArticleView() }
+				{ this._getActicleItemView() }
+			</ScrollView>
 		);
 	}
 }
